8399c5bb5f5d1c18bd5a0c1f945bb527a3e01e2d
Francois G C <38353551+francoisgcouillard@users.noreply.github.com>
2022-03-20 23:15:55 -0400

    Add files via upload

--- code/back/index.php
+++ code/back/index.php
@@ -1,14 +1,17 @@
-<?
+<?php
 //Commencé a 17h50
 //Fini a 19h15
 
+//22h20
+//23h10
+
 define("FILE", "events.json");
 
-if (!isset($_POST["cmd"])) {
+if (!isset($_GET["cmd"])) {
 	http_response_code(400);
 	die();
 }
-if (!in_array($_POST["cmd"], ["add", "list"])) {
+if (!in_array($_GET["cmd"], ["add", "list"])) {
 	http_response_code(406);
 	die();
 }
@@ -28,14 +31,13 @@ $events = json_decode(file_get_contents(FILE), true);
 header('Content-Type: application/json; charset=utf-8');
 
 //Créer un événement
-if ($_POST["cmd"] == "add") {
-	$event = $_POST["event"];
-	
+if ($_GET["cmd"] == "add") {
 	//Validation des données
-	if (!validationCreer($_POST["event"])){
+	if (!validationCreer($_GET["event"])){
 		$response = [ 
 			'success' => false, 
-			'message' => 'Formulaire invalide, le nom a-t-il moins de 32 chars?' 
+			'message' => 'Formulaire invalide, le nom a-t-il moins de 32 chars?',
+			'data' => null
 		];
 	 
 		print json_encode( $response );
@@ -43,13 +45,14 @@ if ($_POST["cmd"] == "add") {
 	}
 	
 	//On ajoute l'événement a la liste
-	$events[] = $event;
+	$events[] = $_GET["event"];
 	
-	file_put_contents (FILE, $events);
+	file_put_contents (FILE, json_encode($events, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
 	
 	$response = [ 
 		'success' => true, 
-		'message' => 'Événement ajouté' 
+		'message' => 'Événement ajouté',
+		'data' => null
 	];
 	 
 	print json_encode( $response );
@@ -61,7 +64,7 @@ else if ($_GET["cmd"] == "list") {
 	$response = [ 
 		'success' => true, 
 		'message' => 'Opération réussie',
-		'data' = $events
+		'data' => $events
 	];
 	 
 	print json_encode( $response );
